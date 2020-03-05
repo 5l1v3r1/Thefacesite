093f0b97b7d482166186c2fee6bf9c0d5236cc95
Step By Step <52529306+stepbystepexe@users.noreply.github.com>
2020-03-05 22:21:45 +0700

    Update thefacesite.py

--- thefacesite.py
+++ thefacesite.py
@@ -4,8 +4,8 @@
 # Coded by Nedi Senja
 # Github: https://github.com/stepbystepexe/Thefacesite
 
-from __opt__ import *
-import os, sys, time, marshal
+import os, sys, time
+from time import sleep
 from urllib2 import Request, urlopen, URLError, HTTPError
 
 info = """
@@ -26,18 +26,18 @@ NB          : Manusia gax ada yang sempurna
 
 [ \033[4mGunakan tool ini dengan bijak \033[0m]\n """
 
-example = """\x1b[103;90;1m[       The Face Site, My Github: @stepbystepexe         ]\033[0m"""
+example = """\x1b[103;90;1m[         Thefacesite, My Github: @stepbystepexe         ]\033[0m"""
+
+logo = """
+ \033[0;92m▀▀█▀▀ █  █ █▀▀▀ \033[0;94m█▀▀▀ █▀▀█ █▀▀▀ █▀▀▀ \033[0;37m█▀▀▀ ▀█▀ ▀▀█▀▀ █▀▀▀
+ \033[0;92m  █   █▀▀█ █▀▀  \033[0;94m█▀▀▀ █▀▀█ █    █▀▀  \033[0;37m▀▀▀█  █    █   █▀▀
+ \033[0;92m  ▀   ▀  ▀ ▀▀▀▀ \033[0;94m▀    ▀  ▀ ▀▀▀▀ ▀▀▀▀ \033[0;37m▀▀▀▀ ▀▀▀   ▀   ▀▀▀▀
+"""
 
 def restart():
     python = sys.executable
     os.execl(python, python, * sys.argv)
     curdir = os.getcwd()
-
-def space(o):
-        x = 0
-        while x <= o:
-                print " ",
-                x += 1
 
 def write(o):
     for i in o + '\n':
@@ -45,14 +45,27 @@ def write(o):
         sys.stdout.flush()
         time.sleep(0.03)
 
+def space(o):
+        x = 0
+        while x <= o:
+                print " ",
+                x += 1
+
 def getlink():
+        os.system('clear')
+        os.system('reset')
+        sleep(1)
+        print
+        print(logo)
+        print(example)
+        print
+        write("\033[0m[ \033[32mINFO \033[0m] \033[3mSaya tidak real kalo code program saya ini ditiru")
+        write("         Seburuk apaun itu tolong hargai karya milik orang\033[0m\n\n")
         x = open("vuln.txt","r");
-        print
         time.sleep(1)
+        link = raw_input('\033[0m[\033[41;77;1m Website \033[0m] ')
         print
-        link = raw_input('\x1b[0m[\x1b[95;1m+\x1b[0m] \x1b[77;1mMasukan Web Target\x1b[0m: ')
-        print
-        write ('\x1b[0m[\x1b[91;1m!\x1b[0m] \x1b[77;1mMengecek Link ...')
+        write("\n\033[0m[\033[91;1m!\033[0m] \033[77;1mMengecek Web Server ...")
         print
         while True:
                 o = x.readline()
@@ -67,24 +80,26 @@ def getlink():
                 except URLError as e:
                         continue
                 else:
-                        print '\x1b[0m[\x1b[94;1m#\x1b[0m] \x1b[77;1;3mMencobak\x1b[0m: ',i
+                        print '\033[0m[\033[94;1m#\033[0m] \033[77;1mMencoba\x1b[0m: ',i
 
 os.system('clear')
 os.system('reset')
-time.sleep(1)
+sleep(1)
 print
 print(logo)
 print(example)
 print
-print("\033[0m[\033[1;96mあ\033[0m] \033[1;77mMulai Menggunakan Thefacesite")
+print("\033[0m[\033[1;96;2m1\033[0m] \033[1;77mDeface Website")
 print
 print("\033[0m[\033[93;1m&\033[0m] LISENSI")
 print("\033[0m[\033[94;1m#\033[0m] Informasi")
 print("\033[0m[\033[92;1m*\033[0m] Perbarui")
 print("\033[0m[\033[91;1m-\033[0m] Keluar")
 print
-option = raw_input("\033[0m[\033[1;95m/\033[0m] \033[1;77mMasukan opsi: \033[0m")
-if option.strip() in 'あ 1 gunakan'.split():
+option = raw_input("\033[0m(\033[105;77;1m/\033[0m) \033[1;77mMasukan opsi: \033[0m")
+if option.strip() in '1 deface'.split():
+    write("\n\033[0m[\033[32m●\033[0m] \033[77;1mSedang memproses ...\033[0m")
+    sleep(1)
     getlink()
 elif option.strip() in '& 2 lisensi'.split():
     print
@@ -96,7 +111,7 @@ elif option.strip() in '# 3 info'.split():
     print(example)
     os.system('toilet -f smslant Deface')
     print(info)
-    time.sleep(1)
+    sleep(1)
     print
     raw_input('[ Tekan Enter ]')
     restart()
@@ -104,14 +119,14 @@ elif option.strip() in '* 4 perbarui'.split():
     print
     os.system('git pull origin master')
     print
-    raw_input('\033[0m[ \033[92mTekan Enter \033[0m]')
+    raw_input('\033[0m[ \033[32mTekan Enter \033[0m]')
     restart()
-elif option.strip() in '- keluar 0'.split():
+elif option.strip() in '- 0 keluar'.split():
     print("\n\033[0m[\033[1;91m!\033[0m] \033[1;77mKeluar dari program!")
     print
     sys.exit(1)
 else:
     print("\n\033[0m[=\033[1;41;77m Pilihan Salah \033[0m=]")
     print
-    time.sleep(1)
+    sleep(1)
     restart()
